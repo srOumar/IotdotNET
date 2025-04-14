ac37bcd4f920b534a7d97376a5efad85abd26e2d
srOumar <sroumar05@gmail.com>
2025-04-14 13:54:09 +0200

    GPIO clignotement ok

---
+++ BlinkTutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Console.WriteLine("Hello, World!");
+//ceci etait le code par défaut
+*/
+
+using System;
+using System.Device.Gpio;
+using System.Threading;
+
+Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+int pin = 23; //pensez à changer en fonction du pin utilisé dans ton circuit
+using var controller = new GpioController();
+controller.OpenPin(pin, PinMode.Output);
+bool ledOn = true;
+while (true)
+{
+    controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+    Thread.Sleep(1000);
+    ledOn = !ledOn;
+}
