b084609a1f16be810193e23c2107786d4fb2d701
matthis <117262353+matthisisback@users.noreply.github.com>
2025-04-14 15:42:32 +0200

    Update Program.cs

--- BlinkTutorial/Program.cs
+++ BlinkTutorial/Program.cs
@@ -1,21 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*
-Console.WriteLine("Hello, World!");
-//ceci etait le code par défaut
-*/
-
 using System;
 using System.Device.Gpio;
-using System.Threading;
+using System.Threading.Tasks;
 
-Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
-int pin = 23; //pensez à changer en fonction du pin utilisé dans ton circuit
+const int PinButton = 24;
+const int PinLed = 23;   
+
 using var controller = new GpioController();
-controller.OpenPin(pin, PinMode.Output);
-bool ledOn = true;
-while (true)
+
+
+controller.OpenPin(PinButton, PinMode.InputPullUp); 
+controller.OpenPin(PinLed, PinMode.Output);       
+
+
+Console.WriteLine(
+    $"({DateTime.Now}) État initial du bouton : {(controller.Read(PinButton) == PinValue.Low ? "Pressé" : "Relâché")}");
+
+// Callback appelé à chaque changement d’état du bouton
+controller.RegisterCallbackForPinValueChangedEvent(
+    PinButton,
+    PinEventTypes.Falling | PinEventTypes.Rising,
+    OnButtonEvent);
+
+await Task.Delay(Timeout.Infinite);
+
+static void OnButtonEvent(object sender, PinValueChangedEventArgs args)
 {
-    controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
-    Thread.Sleep(1000);
-    ledOn = !ledOn;
+    var controller = (GpioController)sender;
+
+    if (args.ChangeType == PinEventTypes.Falling)
+    {
+        controller.Write(PinLed, PinValue.High);
+        Console.WriteLine($"({DateTime.Now}) LED allumée");
+    }
+    else if (args.ChangeType == PinEventTypes.Rising)
+    {
+        controller.Write(PinLed, PinValue.Low);
+        Console.WriteLine($"({DateTime.Now}) LED éteinte");
+    }
 }
